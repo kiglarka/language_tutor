7641fff00e7f76331ad7cfa90ad85a8b1fd0fa96
Bogi Kiss <bogikiss85@gmail.com>
2020-06-10 15:07:56 +0200

    restructure DB architecture

--- my_app/app/src/main/java/com/codecool/languagetutor/db/RoomRepository.java
+++ my_app/app/src/main/java/com/codecool/languagetutor/db/RoomRepository.java
@@ -1,54 +1,24 @@
 package com.codecool.languagetutor.db;
 
 import android.content.Context;
-import android.os.AsyncTask;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
-public class RoomRepository implements FrenchRepository {
+public class RoomRepository {
 
-
-    DictRoomDatabase db;
     private FrenchDao frenchDao;
-    private List<French> allFrench;
 
-    public RoomRepository(Context context){
-        db = DictRoomDatabase.getDatabase(context);
+
+    public RoomRepository(Context context) {
+        this.frenchDao = DictRoomDatabase.getDatabase(context).frenchDao();
     }
 
-
-    @Override
     public void insert(French french) {
-        DictRoomDatabase.databaseWriteExecutor.execute(() -> db.frenchDao().insert(french));
+        frenchDao.insert(french);
     }
 
-
-    @Override
-    public List<French> getAllFrench() {
-        try {
-            return new GetFrenchAsyncTask().execute().get();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public List<French> getAllWords() {
+        return frenchDao.getAlphabetizedTrips();
     }
 
-    private class GetFrenchAsyncTask extends AsyncTask<Void,Void,List<French>>{
-
-        @Override
-        protected List<French> doInBackground(Void... voids) {
-            return db.frenchDao().getAlphabetizedTrips();
-        }
-    }
-
-    @Override
-    public void clearAllFrench() {
-        if (allFrench != null){
-            frenchDao.deleteAll();
-        }
-
-    }
 }
