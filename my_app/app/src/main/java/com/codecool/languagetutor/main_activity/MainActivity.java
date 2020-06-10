13931489d49b9660c871e2d9116b401233c9a420
Bogi Kiss <bogikiss85@gmail.com>
2020-06-10 10:12:20 +0200

    add BindView and setup clickListener on my words button

--- my_app/app/src/main/java/com/codecool/languagetutor/main_activity/MainActivity.java
+++ my_app/app/src/main/java/com/codecool/languagetutor/main_activity/MainActivity.java
@@ -1,12 +1,22 @@
 package com.codecool.languagetutor.main_activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.codecool.languagetutor.R;
+import com.codecool.languagetutor.newword.NewWordActivity;
 
-public class MainActivity extends AppCompatActivity implements MainActivityView{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements MainActivityView {
+
+    @BindView(R.id.btn_list)
+    Button buttonMyWords;
 
     MainActivityPresenter presenter;
 
@@ -15,8 +25,23 @@ public class MainActivity extends AppCompatActivity implements MainActivityView{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         presenter = new MainActivityPresenter();
+
+        setClickListenerOnMyWordsButton();
+
     }
+
+    private void setClickListenerOnMyWordsButton() {
+        buttonMyWords.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 
 }
